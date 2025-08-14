f62b05863d6091cba9fb3fc9abff0db49ee00565
Dipesh Aryal <aryaldeelep@gmail.com>
2025-08-14 07:48:32 +0545

    chore: set up navigation and nativewind

--- moviez/tailwind.config.js
+++ moviez/tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx",
+  content: [
+    "./App.tsx",
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"],
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary : "#030014",
+        accent :"#AB8BFF",
+        secondary :'#151312',
+        light:{
+          100:"#D6C6FF",
+          200:"#a8b5db",
+          300:"#9ca4ab",
+        },
+        dark:{
+          100:"#221f3d",
+          200:"#0f0d23",
+          300:"#",   
+        }
+      }
+    },
   },
   plugins: [],
-}
+};
